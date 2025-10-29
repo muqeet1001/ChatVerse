eea3f63017d55b8870002c7d70109672bb65a1e2
muqeet1001 <abdul1002muqeet@gmail.com>
2025-10-29 22:02:24 +0530

    Refactor message handling to maintain chat history in AI response generation

--- server.js
+++ server.js
@@ -4,22 +4,29 @@ const { createServer } = require("http");
 const { Server } = require("socket.io");
 const { generateResponse } = require("./src/service/ai.service");
 const { Socket } = require("dgram");
+const { text } = require("stream/consumers");
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   /* options */
 });
- 
-  io.on("connection", (socket) => {
+const chatHistory = [];
+io.on("connection", (socket) => {
   console.log("A client connected");
 
-  socket.on("chat message", async(msg) => {
+  socket.on("chat message", async (msg) => {
     console.log("Message from client:", msg);
-    const reply =  await generateResponse(msg);
+    chatHistory.push({
+      role: "user",
+      parts: [{ text: msg }],
+    });
+    const reply = await generateResponse(chatHistory);
     socket.emit("reply", "Got your message: " + reply);
+    chatHistory.push({
+      role: "assistant",
+      parts: [{ text: reply }],
+    });
   });
 });
-
-
 
 httpServer.listen(3000, () => {
   console.log("Server is running on port 3000");
--- src/service/ai.service.js
+++ src/service/ai.service.js
@@ -2,10 +2,10 @@
 
 const ai = new GoogleGenAI({ apiKey: "AIzaSyBKsOm_5Yw30D1OSVyI1_t55emeDCt-ny0" });
 
-async function generateResponse(prompt) {
+async function generateResponse(chatHistory) {
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
-    contents: prompt,
+    contents: chatHistory,
   });
   return response.text;
 }
